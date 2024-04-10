3c3c48035d8b17006682a29d001facfdd27a93ff
DRACKRAGE <brunosouzacc@hotmail.com>
2024-04-10 16:59:48 -0300

    Altera server.js

--- src/server.js
+++ src/server.js
@@ -18,5 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
+  console.log("Boas vindas a API do Allbooks")
+  console.log("API disponível através da http://localhost:8000")
 })
