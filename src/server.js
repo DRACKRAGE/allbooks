64240cec77e18d9a25e40a1b81f3a7c7a7dc3cf6
DRACKRAGE <86166421+DRACKRAGE@users.noreply.github.com>
2024-04-10 16:57:31 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -18,5 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
+  console.log("Acesse essa API em http://localhost:8000")
 })
